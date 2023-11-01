de5dac016a11ec0c7a749601558e63829b8b1ec3
Collin Kidder <collink@kkmfg.com>
2023-10-31 22:04:32 -0400

    New device driver that will output status entries on the second serial port at a configurable rate in CSV format. This allows for easy logging over USB of just the parameters you want to log.

---
+++ src/devices/display/StatusCSV.cpp
@@ -0,0 +1,261 @@
+/*
+ * StatusCSV.cpp
+ *
+ * Create configurable status output on SerialUSB1
+ *
+ Copyright (c) 2023 Collin Kidder
+
+ Permission is hereby granted, free of charge, to any person obtaining
+ a copy of this software and associated documentation files (the
+ "Software"), to deal in the Software without restriction, including
+ without limitation the rights to use, copy, modify, merge, publish,
+ distribute, sublicense, and/or sell copies of the Software, and to
+ permit persons to whom the Software is furnished to do so, subject to
+ the following conditions:
+
+ The above copyright notice and this permission notice shall be included
+ in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ */
+
+#include "StatusCSV.h"
+#include "CanHandler.h"
+
+/*
+ * Constructor.
+ */
+StatusCSV::StatusCSV() : Device()
+{
+    commonName = "Status output in CSV format";
+    shortName = "StatusCSV";
+    config = nullptr;
+    tickCounter = 0;
+    haveEnabledEntries = false;
+    isEnabled = false;
+}
+
+void StatusCSV::earlyInit()
+{
+    prefsHandler = new PrefHandler(STATUSCSV);
+}
+
+/*
+ * Initialization of hardware and parameters
+ */
+void StatusCSV::setup() {
+
+    Logger::info("add device: EVIC (id: %X, %X)", STATUSCSV, this);
+
+    tickHandler.detach(this);//Turn off tickhandler
+
+    loadConfiguration(); //Retrieve any persistant variables
+
+    Device::setup(); // run the parent class version of this function
+
+    handleSerialSwitch(); //see if GVRET output should be turned off
+
+    ConfigEntry entry;
+    entry = {"TICKUPDATE", "Set number of timer ticks per update (20ms intervals)", &config->ticksPerUpdate, CFG_ENTRY_VAR_TYPE::UINT16, {.u_int = 1}, {.u_int = 10000}, 1, nullptr};
+    cfgEntries.push_back(entry);
+    entry = {"STATUS-EN", "Specify a status entry hash to enable", &config->enableHash, CFG_ENTRY_VAR_TYPE::UINT32, {.u_int = 1}, {.u_int = 0xFFFFFFFF}, 1, nullptr};
+    cfgEntries.push_back(entry);
+    entry = {"STATUS-DIS", "Specify a status entry hash to disable", &config->disableHash, CFG_ENTRY_VAR_TYPE::UINT32, {.u_int = 1}, {.u_int = 0xFFFFFFFF}, 1, nullptr};
+    cfgEntries.push_back(entry);
+
+    tickHandler.attach(this, CFG_TICK_INTERVAL_STATUS);
+
+    config->enableHash = 0;
+    config->disableHash = 0;
+}
+
+//This method handles periodic tick calls received from the tasker.
+void StatusCSV::handleTick()
+{
+    StatusEntry *ent = nullptr;
+    int c;
+
+    if (config->enableHash)
+    {
+        ent = deviceManager.findStatusEntryByHash(config->enableHash);
+        if (ent)
+        {
+            enableStatusHash(config->enableHash);
+            isEnabled = false;
+            Logger::console("Enabled the status entry!");
+        }
+        config->enableHash = 0;
+    }
+    if (config->disableHash)
+    {
+        ent = deviceManager.findStatusEntryByHash(config->disableHash);
+        if (ent)
+        {
+            disableStatusHash(config->disableHash);
+            isEnabled = false;
+            Logger::console("Disabled the status entry!");
+        }
+        config->disableHash = 0;
+    }
+
+    while (SerialUSB1.available()) {
+        c = SerialUSB1.read();
+        if ((c == 's') || (c =='S'))
+        {
+            isEnabled = !isEnabled;
+            if (isEnabled)
+            {
+                for (int i = 0; i < NUM_ENTRIES_IN_TABLE; i++)
+                {
+                    if (!config->enabledStatusEntries[i]) continue;
+                    ent = deviceManager.findStatusEntryByHash(config->enabledStatusEntries[i]);
+                    if (ent)
+                    {
+                        SerialUSB1.print(ent->statusName);
+                        SerialUSB1.write(',');
+                    }
+                }
+                SerialUSB1.println();
+            }
+        }
+    }
+
+    if (!isEnabled) return;
+    if (++tickCounter > config->ticksPerUpdate)
+    {
+        tickCounter = 0;
+        //do processing
+        for (int i = 0; i < NUM_ENTRIES_IN_TABLE; i++)
+        {
+            if (!config->enabledStatusEntries[i]) continue;
+            ent = deviceManager.findStatusEntryByHash(config->enabledStatusEntries[i]);
+            if (ent)
+            {
+                SerialUSB1.print(ent->getValueAsString());
+                SerialUSB1.write(',');
+            }
+        }
+        SerialUSB1.println();
+    }
+}
+
+void StatusCSV::enableStatusHash(uint32_t hash)
+{
+    for (int i = 0; i < NUM_ENTRIES_IN_TABLE; i++)
+    {
+        if (config->enabledStatusEntries[i] == 0)
+        {
+            config->enabledStatusEntries[i] = hash;
+            return;
+        }
+    }
+    saveConfiguration();
+    handleSerialSwitch();
+}
+
+void StatusCSV::disableStatusHash(uint32_t hash)
+{
+    for (int i = 0; i < NUM_ENTRIES_IN_TABLE; i++)
+    {
+        if (config->enabledStatusEntries[i] == hash)
+        {
+            config->enabledStatusEntries[i] = 0;
+            //return;
+        }
+    }
+    saveConfiguration();
+    handleSerialSwitch();
+}
+
+/*
+The idea here is to see if we have any enabled status outputs. If we do and we've loaded this class then we will
+make the second serial port our output. That means we have to turn off GVRET output on that serial port. Otherwise
+we want to make sure GVRET mode is enable for that serial port.
+This gives the code a dependency on CanHandler even though we're not sending any CAN (currently)
+*/
+void StatusCSV::handleSerialSwitch()
+{
+    bool anythingenabled = false;
+    for (int i = 0; i < NUM_ENTRIES_IN_TABLE; i++)
+    {
+        if (config->enabledStatusEntries[i] > 0) anythingenabled = true;
+    }
+    if (anythingenabled)
+    {
+        canHandlerBus0.setGVRETMode(false);
+        canHandlerBus1.setGVRETMode(false);
+        canHandlerBus1.setGVRETMode(false);
+        haveEnabledEntries = true;
+    }
+    else //this presupposes that we're the only one who could take over the second serial port. Currently that is correct.
+    {
+        canHandlerBus0.setGVRETMode(true);
+        canHandlerBus1.setGVRETMode(true);
+        canHandlerBus1.setGVRETMode(true);
+        haveEnabledEntries = false;
+    }
+}
+
+DeviceType StatusCSV::getType() {
+    return DEVICE_MISC;
+}
+
+DeviceId StatusCSV::getId() {
+    return (STATUSCSV);
+}
+
+bool StatusCSV::isHashMonitored(uint32_t hash)
+{
+    for (int i = 0; i < NUM_ENTRIES_IN_TABLE; i++)
+    {
+        if (config->enabledStatusEntries[i] == hash) return true;
+    }
+    return false;
+}
+
+void StatusCSV::loadConfiguration() {
+    config = (StatusCSVConfiguration *)getConfiguration();
+
+    if (!config) {
+        config = new StatusCSVConfiguration();
+        setConfiguration(config);
+    }
+
+    Device::loadConfiguration(); // call parent
+
+    prefsHandler->read("TicksPer", &config->ticksPerUpdate, 5);
+    //if block hasn't ever been written to EEPROM then just default it to all zeros
+    if (!prefsHandler->readBlock("EnabledItems", (uint8_t *)&config->enabledStatusEntries, sizeof(config->enabledStatusEntries)))
+    {
+        memset((uint8_t *)&config->enabledStatusEntries, 0, sizeof(config->enabledStatusEntries));
+        //these are test entries. Don't uncomment these lines for production builds
+        //config->enabledStatusEntries[0] = 0xe4d7dd0c;
+        //config->enabledStatusEntries[1] = 0x31ea2b88;
+        //config->enabledStatusEntries[2] = 0x77249773;
+    }
+}
+
+void StatusCSV::saveConfiguration() {
+    if (!config) {
+        config = new StatusCSVConfiguration();
+        setConfiguration(config);
+    }
+
+    prefsHandler->write("TicksPer", config->ticksPerUpdate);
+    if (!prefsHandler->writeBlock("EnabledItems", (uint8_t *)&config->enabledStatusEntries, sizeof(config->enabledStatusEntries)))
+    {
+        Logger::error("Could not write enabled status fields register!");
+    }
+
+    prefsHandler->saveChecksum();
+    prefsHandler->forceCacheWrite();
+}
+
+StatusCSV statuscsv;
---
+++ src/devices/display/StatusCSV.h
@@ -0,0 +1,83 @@
+/*
+ * StatusCSV.h
+ *
+ * Outputs CSV formatted lines to the second serial port at a configurable rate
+ *
+ *
+ * Created: Oct 2023
+  *  Author: Collin Kidder
+
+ Copyright (c) 2023 Collin Kidder
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#ifndef STATUSCSV_H_
+#define STATUSCSV_H_
+
+#include <Arduino.h>
+#include "../../config.h"
+#include "../../constants.h"
+#include "../Device.h"
+#include "../../TickHandler.h"
+#include "../../DeviceManager.h"
+#include "../../Sys_Messages.h"
+#include "../DeviceTypes.h"
+
+#define STATUSCSV                   0x4500
+#define CFG_TICK_INTERVAL_STATUS    20000
+#define NUM_ENTRIES_IN_TABLE        40
+
+class StatusCSVConfiguration: public DeviceConfiguration {
+public:
+    uint16_t ticksPerUpdate;
+    uint32_t enabledStatusEntries[NUM_ENTRIES_IN_TABLE]; //4 bytes per entry
+    uint32_t enableHash;
+    uint32_t disableHash;
+};
+
+class StatusCSV: public Device {
+public:
+
+    StatusCSV();
+    virtual void handleTick();
+
+    virtual void setup(); //initialization on start up
+    void earlyInit();
+    DeviceType getType();
+    DeviceId getId();
+    bool isHashMonitored(uint32_t hash);
+
+    void loadConfiguration();
+    void saveConfiguration();
+
+private:
+    StatusCSVConfiguration *config;
+    uint32_t tickCounter;
+    bool haveEnabledEntries;
+    bool isEnabled;
+
+    void handleSerialSwitch();
+    void enableStatusHash(uint32_t hash);
+    void disableStatusHash(uint32_t hash);
+};
+
+#endif
+
